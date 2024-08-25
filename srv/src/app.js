c41bcfbff4b2cf55a92f4ed1517ecd8f4dce0168
danielrocha28 <danielrocha2805@gmail.com>
2024-08-24 22:54:04 -0300

    update estrutura do projeto para IPV4

---
+++ srv/src/app.js
@@ -0,0 +1,35 @@
+const Fastify = require('fastify')
+const fastifyCors = require('@fastify/cors')
+
+const fastify = Fastify()
+
+// Importar rotas
+
+const userRoutes = require('./routes/user.routes')
+
+
+// Registrar plugins
+fastify.register(fastifyCors, {
+  // Opções de configuração do CORS
+  origin: '*', // Permitir todas as origens
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+})
+
+// Registrar rotas
+
+fastify.register(userRoutes)
+
+// Iniciar o servidor
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 }) // Substitua pela porta que desejar
+    console.log(`Servidor rodando na porta 3000`)
+  } catch (err) {
+    console.error(err)
+    process.exit(1)
+  }
+}
+
+start()
+
+module.exports = fastify
