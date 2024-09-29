92c91075f25b5c0138817731f5e77b79cc53242b
IsabelCarvalh <163677970+IsabelCarvalh@users.noreply.github.com>
2024-09-29 20:17:14 -0300

    Update Containers.dart

--- kairoz.app/lib/widgets/Containers.dart
+++ kairoz.app/lib/widgets/Containers.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:kairoz_rascunho/Telainformacoes.dart';
+import 'Telainformacoes.dart';
 
 class MyCarousel extends StatelessWidget {
   @override
