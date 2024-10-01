cee516c987db2bd6b71bdb1bcc22e762febb7f42
Diegotavio <diegotavio882@gmail.com>
2024-10-01 03:35:28 -0300

    feat/Rota-index funcionando

--- srv/src/routes/home.routes.js
+++ srv/src/routes/home.routes.js
@@ -1,5 +1,5 @@
 export default async function homeRouter(fastify, options) {
-  fastify.get('/kairoz.onrender.com/', async (request, reply) => {
+  fastify.get('/', async (request, reply) => {
     return 'Bem vindo ao app Kairoz';
   });
 }
