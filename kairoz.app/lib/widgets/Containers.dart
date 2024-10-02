0e9bc8ff497a1d6d594c096ff7e0e5c4119d17dc
IsabelCarvalh <163677970+IsabelCarvalh@users.noreply.github.com>
2024-10-02 19:37:58 -0300

    Update Containers.dart
    dei update no nome da tela de informações

--- kairoz.app/lib/widgets/Containers.dart
+++ kairoz.app/lib/widgets/Containers.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'Telainformacoes.dart';
+import 'Tela_informacoes.dart';
 
 class Containers extends StatelessWidget {
   @override
