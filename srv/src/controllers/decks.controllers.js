d9a501d6359261d3146c835ef0110c78581eb69a
Lucca-Pericles <163687798+Lucca-Pericles@users.noreply.github.com>
2024-10-10 20:36:27 -0300

    atualizando pull request

--- srv/src/controllers/decks.controllers.js
+++ srv/src/controllers/decks.controllers.js
@@ -2,26 +2,20 @@ import decks from '../model/decks.model.js';
 import deckSchema from '../validators/decks.schema.js';
 import { z } from 'zod';
 
-console.log(deckSchema); // Log the deck schema for debugging purposes
-
-// Function to create new decks
 export async function createDecks(request, reply) {
     try {
-        // Validating the request body against the deck schema
+        
         const validatedData = deckSchema.parse(request.body);
         const { name, description } = validatedData;
 
-        // Creating a new deck in the database
         const newDecks = await decks.create({ name, description });
         reply.status(201).send(newDecks); 
-        //Responding with the created deck
 
     } catch (error) {
-        // Handling validation errors from Zod
         if (error instanceof z.ZodError) {
             return reply.status(400).send({
                 error: 'Validation failed with Zod',
-                details: error.errors // Sending back validation error details
+                details: error.errors 
             });
         } else {
             // Logging internal errors and responding with a generic error message
@@ -34,8 +28,7 @@ export async function createDecks(request, reply) {
     }
 };
 
-// Function to retrieve all decks
-export async function getDecks(request, reply) {
+export async function allDecks(request, reply) {
     try {
         const decksList = await decks.findAll(); // Find all decks in the database
         reply.status(200).send(decksList); // Responding with the list of decks
