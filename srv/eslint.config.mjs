1c30bd30eec8f8d5674fcb62560215b8d6835b49
Gabriel Rocha de Melo <42704380+gabriel1305rocha@users.noreply.github.com>
2024-10-17 01:46:58 +0000

    Update Packages and EsLint to Workflow

--- srv/eslint.config.mjs
+++ srv/eslint.config.mjs
@@ -1,39 +1,55 @@
-import globals from 'globals';
 import pluginJs from '@eslint/js';
 import pluginImport from 'eslint-plugin-import';
+import globals from 'globals';
 
 export default [
   {
+    // Define language options, including global variables and parser settings
     languageOptions: {
       globals: {
-        // Including Node.js and browser global variables
+        // Add Node.js and browser global variables
         ...globals.node,
         ...globals.browser,
       },
       parserOptions: {
-        // ECMAScript 2022 syntax support
+        // Support ECMAScript 2022 syntax
         ecmaVersion: 2022,
-        // Setting source type to 'module' for ES Modules
+        // Set source type to 'module' for ES Modules
         sourceType: 'module',
       },
     },
+    // Register the ESLint plugin for import rules
     plugins: {
-      // Definindo plugins como objetos
       import: pluginImport,
     },
+    // Define custom linting rules
     rules: {
-      // Define rules directly
+      // Warn on console usage
       'no-console': 'warn',
+      // Enforce strict equality checks (=== and !==)
       'eqeqeq': 'error',
+      // Disallow unnecessary 'return await'
       'no-return-await': 'error',
+      // Ensure that errors are returned only as Error objects in promises
       'prefer-promise-reject-errors': 'error',
+      // Disallow async functions without await expressions
       'require-await': 'error',
+      // Warn on unused variables, ignoring function arguments
       'no-unused-vars': ['warn', { args: 'none', ignoreRestSiblings: true }],
+      // Enforce single quotes for strings
       'quotes': ['error', 'single'],
+      // Require semicolons at the end of statements
       'semi': ['error', 'always'],
     },
   },
-  // Usar o pluginJs e pluginImport diretamente no array
-  pluginJs.configs.recommended,
-  pluginImport.configs.recommended,
+  {
+    // Apply recommended ESLint and import plugin rules to all .js files
+    files: ['**/*.js'],
+    rules: {
+      // Include recommended ESLint rules
+      ...pluginJs.configs.recommended.rules,
+      // Include recommended import plugin rules
+      ...pluginImport.configs.recommended.rules,
+    },
+  },
 ];
