0be1344ab4d22453ea21ed5fc267e4690089bcb8
Gabriel Rocha de Melo <42704380+gabriel1305rocha@users.noreply.github.com>
2024-10-16 21:51:54 -0300

    Update eslint.config.mjs

--- srv/eslint.config.mjs
+++ srv/eslint.config.mjs
@@ -4,22 +4,45 @@ import pluginJs from '@eslint/js';
 export default {
   languageOptions: {
     globals: {
+      // Including Node.js and browser global variables
       ...globals.node,
       ...globals.browser
     },
     parserOptions: {
-      ecmaVersion: 2020,
+      // ECMAScript 2022 syntax support
+      ecmaVersion: 2022,
+      // Setting source type to 'module' for ES Modules
       sourceType: 'module'
     },
   },
   extends: [
-    pluginJs.configs.recommended
+    // Extending the recommended ESLint configurations
+    pluginJs.configs.recommended,
+    // Ensuring correct order of imports and handling import errors
+    'plugin:import/recommended'
   ],
   env: {
+    // Enabling environment support for Node.js
     node: true,
-    // adicione outros ambientes conforme necessário
+    // Enabling environment support for browser
+    browser: true,
   },
   rules: {
-    // Adicione suas regras personalizadas aqui
+    // Warn on console usage
+    'no-console': 'warn',
+    // Enforce strict equality checks (=== and !==)
+    'eqeqeq': 'error',
+    // Disallow returning await inside functions
+    'no-return-await': 'error',
+    // Ensure that promises are rejected with an error object
+    'prefer-promise-reject-errors': 'error',
+    // Require await in async functions to avoid errors
+    'require-await': 'error',
+    // Warn when there are unused variables in the code
+    'no-unused-vars': ['warn', { 'args': 'none', 'ignoreRestSiblings': true }],
+    // Enforce the use of single quotes for strings
+    'quotes': ['error', 'single'],
+    // Enforce the use of semicolons at the end of statements
+    'semi': ['error', 'always']
   }
 };
