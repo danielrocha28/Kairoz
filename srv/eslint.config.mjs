ed4b7555fb25fc6baa67aba1486a07380b3c8444
Gabriel Rocha de Melo <42704380+gabriel1305rocha@users.noreply.github.com>
2024-10-17 01:28:57 +0000

    Update EsLint

--- srv/eslint.config.mjs
+++ srv/eslint.config.mjs
@@ -1,42 +1,35 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
+import pluginImport from 'eslint-plugin-import';
 
-export default {
-  languageOptions: {
-    globals: {
-      // Including Node.js and browser global variables
-      ...globals.node,
-      ...globals.browser
+export default [
+  {
+    languageOptions: {
+      globals: {
+        // Including Node.js and browser global variables
+        ...globals.node,
+        ...globals.browser
+      },
+      parserOptions: {
+        // ECMAScript 2022 syntax support
+        ecmaVersion: 2022,
+        // Setting source type to 'module' for ES Modules
+        sourceType: 'module'
+      },
     },
-    parserOptions: {
-      // ECMAScript 2022 syntax support
-      ecmaVersion: 2022,
-      // Setting source type to 'module' for ES Modules
-      sourceType: 'module'
-    },
+    rules: {
+      // Define rules directly
+      'no-console': 'warn',
+      'eqeqeq': 'error',
+      'no-return-await': 'error',
+      'prefer-promise-reject-errors': 'error',
+      'require-await': 'error',
+      'no-unused-vars': ['warn', { 'args': 'none', 'ignoreRestSiblings': true }],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always']
+    }
   },
-  extends: [
-    // Extending the recommended ESLint configurations
-    pluginJs.configs.recommended,
-    // Ensuring correct order of imports and handling import errors
-    'plugin:import/recommended'
-  ],
-  rules: {
-    // Warn on console usage
-    'no-console': 'warn',
-    // Enforce strict equality checks (=== and !==)
-    'eqeqeq': 'error',
-    // Disallow returning await inside functions
-    'no-return-await': 'error',
-    // Ensure that promises are rejected with an error object
-    'prefer-promise-reject-errors': 'error',
-    // Require await in async functions to avoid errors
-    'require-await': 'error',
-    // Warn when there are unused variables in the code
-    'no-unused-vars': ['warn', { 'args': 'none', 'ignoreRestSiblings': true }],
-    // Enforce the use of single quotes for strings
-    'quotes': ['error', 'single'],
-    // Enforce the use of semicolons at the end of statements
-    'semi': ['error', 'always']
-  }
-};
+  // Include recommended rules from the plugins directly
+  pluginJs.configs.recommended,
+  pluginImport.configs.recommended
+];
