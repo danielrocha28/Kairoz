5c5eba1057eac00b44bd83671fd03e378ec73d3b
danielrocha28 <danielrocha.2805@gmail.com>
2024-10-17 22:53:51 -0300

    Delete kairoz.app

--- kairoz.app/lib/app_colors/app_colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color kairozWhite = Color(0xffE4E1F3);
-const Color kairozLightPurple = Color(0xffCFC9F1);
-const Color kairozPurple = Color(0xff958DC5);
-const Color kairozDarkPurple = Color(0xff715398);
