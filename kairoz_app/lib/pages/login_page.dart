753b537c16768af6441eca350ce3d446cd256db2
Gabriel <gabrieldesouzalm3@gmail.com>
2024-11-11 18:45:53 -0300

    Criação das telas e NavBar

--- kairoz_app/lib/pages/login_page.dart
+++ kairoz_app/lib/pages/login_page.dart
@@ -8,10 +8,10 @@ class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
-  _LoginPageState createState() => _LoginPageState();
+  LoginPageState createState() => LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class LoginPageState extends State<LoginPage> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
