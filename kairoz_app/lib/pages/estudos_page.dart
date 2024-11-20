ad756ead9e384a98ced9284c68d436afdd3da9d9
lala <laviniagabriele.jade@gmial.com>
2024-11-19 21:57:33 -0300

    adicao widget agenda

--- kairoz_app/lib/pages/estudos_page.dart
+++ kairoz_app/lib/pages/estudos_page.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:kairoz/widgets/agenda.dart';
 
 class EstudosPage extends StatefulWidget {
   const EstudosPage({super.key});
 
   @override
-  State<EstudosPage> createState() => _MyWidgetState();
+  State<EstudosPage> createState() => _EstudosPageState();
 }
 
-class _MyWidgetState extends State<EstudosPage> {
+class _EstudosPageState extends State<EstudosPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Bem-vindo à página de Estudos!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Página de Estudos'),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: CalendarWidget(),
+      ),
     );
   }
 }
