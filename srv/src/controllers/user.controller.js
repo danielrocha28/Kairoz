1d6090c09c927020df7a502123542b7c23fda4e1
Gabriel <gabrieldesouzalm3@gmail.com>
2024-11-27 16:29:35 -0300

    chore: adjusted to get user by token

--- srv/src/controllers/user.controller.js
+++ srv/src/controllers/user.controller.js
@@ -89,14 +89,18 @@ export async function loginUser(request, reply) {
   }
 }
 
-export const getUserByID = async (id_user) => {
-    const user = await User.findOne({ where: { id_user } });
+export const getUserByID = async (token) => {
+  const decodedUser = jwt.decode(token);
+    
+  const user = await User.findOne({ where: {
+    id_user: decodedUser.id }
+  });
 
-    return {
-      id: user.id_user,
-      name: user.name,
-      email: user.email,
-    };
+  return {
+    id: user.id_user,
+    name: user.name,
+    email: user.email,
+  };
 };
 
 export async function updateProfile(request, reply){
