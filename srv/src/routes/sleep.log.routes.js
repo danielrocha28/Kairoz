48e158bed42c25bac8062158c845d936d67dfef6
DIEGO OTAVIO ALVES FERREIRA <23330@alunos.etec.itaqua>
2024-12-05 21:06:55 -0300

    feat/sleep-log criando as rotas do registro de sono

---
+++ srv/src/routes/sleep.log.routes.js
@@ -0,0 +1,38 @@
+import logger from '../config/logger';
+import { getUserByID } from '../controllers/user.controller';
+import sleepLogSchema from '../validators/sleep.log.schema';
+
+const sleepLogRouters = (fastify, options, done) => {
+
+    fastify.post('sleep-time', async (request, reply) => {
+        const token = (request.headers.authorization?.split(' ') ?? [])[1];
+        const user = await getUserByID(token);
+        try {
+           const validatedData = sleepLogSchema.parse(request.body);
+           const newSleepTime = await SleepTime.create(validatedData, { id_user: user.id });
+           reply.status(201).send(newSleepTime);
+        } catch (error) {
+            logger.error(error);
+            reply.status(500).send({ error: 'Error processing request', details: error.message });
+        }
+    });
+
+    fastify.get('sleep_time', async (request, reply) => {
+        const token = (request.headers.authorization?.split(' ') ?? [])[1];
+        const user = await getUserByID(token);
+        try {
+            const findSleepTime = await SleepTime.findOne({ where: { id_user: user.id }});
+            if (!findSleepTime) {
+                reply.status(404).send('no sleep log found');
+            }
+            reply.status(200).send(findSleepTime);
+        } catch (error) {
+            logger.error(error);
+            reply.status(500).send({ error: 'Error processing request', details: error.message });
+        }
+    });
+
+    done();
+};
+
+export default sleepLogRouters
---
+++ srv/src/validators/sleep.log.schema.js
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+const sleepLogSchema = z.object({
+    sleep_time: z.string().regex(/^\d{2}:\d{2}$/, {
+        message: `O formato deve ser '00:00', com dois dígitos para horas e dois para minutos.`,
+      }),
+    wake_up_time: z.string().regex(/^\d{2}:\d{2}$/, {
+        message: `O formato deve ser '00:00', com dois dígitos para horas e dois para minutos.`,
+      }),
+});
+
+export default sleepLogSchema;
